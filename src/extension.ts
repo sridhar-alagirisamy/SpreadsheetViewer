c1e2fbe2a4e6a43bfccff3c24fcff64b3701301e
madhan <madhan.venkateshan@syncfusion.com>
2019-12-26 14:35:37 +0530

    EJ2 spreadsheet intregrated

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -17,11 +18,37 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello VSCode!');
+		//vscode.window.showInformationMessage('Hello VSCode!');
+		const panel = vscode.window.createWebviewPanel('spreadsheet', 'Spreadsheet Viewer', vscode.ViewColumn.One, { enableScripts: true,
+			localResourceRoots: [vscode.Uri.file(path.join(context.extensionPath, 'scripts'))] });
+		panel.webview.html = getWebviewContent();
 	});
 
 	context.subscriptions.push(disposable);
 }
+
+function getWebviewContent() {
+	return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+	  <meta charset="UTF-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	  <title>Spreadsheet Viewer</title>
+	  <script src="https://cdn.syncfusion.com/ej2/17.4.39/dist/ej2.min.js" type="text/javascript"></script>
+      <link href="https://cdn.syncfusion.com/ej2/material.css" rel="stylesheet">
+  </head>
+  <body>
+		  <div id="spreadsheet"></div>
+	<script>
+	var spreadsheet = new ej.spreadsheet.Spreadsheet({
+        openUrl: 'https://ej2services.syncfusion.com/production/web-services/api/spreadsheet/open',
+        saveUrl: 'https://ej2services.syncfusion.com/production/web-services/api/spreadsheet/save'
+    });
+	spreadsheet.appendTo('#spreadsheet');
+	</script>
+  </body>
+  </html>`;
+  }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
