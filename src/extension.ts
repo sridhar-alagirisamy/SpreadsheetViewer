6fc1aaf11769ccedd243d7877661f9f229fe136c
Sridhar Alagiri Samy <sridhar.alagirisamy@syncfusion.com>
2019-12-26 15:28:57 +0530

    Spreadsheet full screen and resize handling

--- src/extension.ts
+++ src/extension.ts
@@ -35,16 +35,30 @@ function getWebviewContent() {
 	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	  <title>Spreadsheet Viewer</title>
 	  <script src="https://cdn.syncfusion.com/ej2/17.4.39/dist/ej2.min.js" type="text/javascript"></script>
-      <link href="https://cdn.syncfusion.com/ej2/material.css" rel="stylesheet">
+	  <link href="https://cdn.syncfusion.com/ej2/material.css" rel="stylesheet">
+	  <style>
+	  body {
+		overflow: hidden;
+		margin: 0;
+	  }
+	  </style>
   </head>
   <body>
-		  <div id="spreadsheet"></div>
+	<div id="spreadsheet"></div>
 	<script>
+	document.body.style.height = document.documentElement.clientHeight + 'px';
+	
 	var spreadsheet = new ej.spreadsheet.Spreadsheet({
         openUrl: 'https://ej2services.syncfusion.com/production/web-services/api/spreadsheet/open',
         saveUrl: 'https://ej2services.syncfusion.com/production/web-services/api/spreadsheet/save'
     });
 	spreadsheet.appendTo('#spreadsheet');
+
+	window.addEventListener('resize', onResize);
+	function onResize() {
+    	document.body.style.height = document.documentElement.clientHeight + 'px';
+    	spreadsheet.resize();
+	}
 	</script>
   </body>
   </html>`;
